9daa3efe2ce426836ed5eb21ce88339acd20fc95
kanewi11 <blacknekit11@gmail.com>
2023-11-30 12:12:43 +0300

    small improvements

--- airplane_arrival/airplane_arrival.py
+++ airplane_arrival/airplane_arrival.py
@@ -2,9 +2,9 @@ from .arrival_time import ArrivalTime
 
 
 class AirplaneArrival:
-    text_delayed = 'Самолет опаздывает. Задержка: {0}'
-    text_ahead = 'Самолет прилетел раньше. Опережение: {0}'
-    text_in_time = 'Самолет прилетел вовремя: {0}'
+    _text_delayed = 'Самолет опаздывает. Задержка: {0}'
+    _text_ahead = 'Самолет прилетел раньше. Опережение: {0}'
+    _text_in_time = 'Самолет прилетел вовремя: {0}'
 
     def __init__(self, scheduled_arrival: str, actual_arrival: str):
         self.scheduled_arrival = ArrivalTime(scheduled_arrival)
@@ -13,14 +13,14 @@ class AirplaneArrival:
     def get_result(self) -> str:
         if self.actual_arrival > self.scheduled_arrival:
             delay = self.actual_arrival - self.scheduled_arrival
-            return self.text_delayed.format(delay)
+            return self._text_delayed.format(delay)
         elif self.actual_arrival < self.scheduled_arrival:
             ahead_of_schedule = self.scheduled_arrival - self.actual_arrival
-            return self.text_ahead.format(ahead_of_schedule)
-        return self.text_in_time.format(self.scheduled_arrival)
+            return self._text_ahead.format(ahead_of_schedule)
+        return self._text_in_time.format(self.scheduled_arrival)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Время по расписанию: {self.scheduled_arrival} | Фактическое время: {self.actual_arrival}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.__class__.__name__}({repr(self.scheduled_arrival)}, {repr(self.actual_arrival)})'
--- airplane_arrival/arrival_time.py
+++ airplane_arrival/arrival_time.py
@@ -52,5 +52,5 @@ class ArrivalTime:
     def __str__(self) -> str:
         return str(self.arrival_time.time())[:5]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.__class__.__name__}({repr(self.arrival_time)})'
