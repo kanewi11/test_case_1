dcc9b5c129ddd4c31b402b19809ce6fd03a36322
kanewi11 <blacknekit11@gmail.com>
2023-11-30 12:00:16 +0300

    incorrect time output was fixed and timedelta support in ArrivalTime class was removed

--- airplane_arrival/arrival_time.py
+++ airplane_arrival/arrival_time.py
@@ -9,8 +9,8 @@ class ArrivalTime:
     _PATTERN = re.compile('[0-9]{2}:[0-9]{2}')
     _FORMAT = '%H:%M'
 
-    def __init__(self, arrival_time: Union[str, datetime, timedelta]):
-        if not isinstance(arrival_time, (datetime, timedelta)):
+    def __init__(self, arrival_time: Union[str, datetime]):
+        if not isinstance(arrival_time, datetime):
             self._check_valid_arrival(arrival_time)
             self.arrival_time = self._str_to_time_obj(arrival_time)
         else:
@@ -23,11 +23,14 @@ class ArrivalTime:
     def _str_to_time_obj(self, arrival: str) -> datetime:
         return datetime.strptime(arrival, self._FORMAT)
 
-    @classmethod
-    def __verify_data(cls, other) -> datetime:
+    @staticmethod
+    def _datetime_to_timedelta(dt: datetime) -> timedelta:
+        return timedelta(hours=dt.hour, minutes=dt.minute)
+
+    @staticmethod
+    def __verify_data(other: Union['ArrivalTime', time]) -> datetime:
         if not isinstance(other, (time, ArrivalTime)):
             raise ArrivalTypeError
-
         return other if isinstance(other, (time, datetime)) else other.arrival_time
 
     def __lt__(self, other: Union['ArrivalTime', time]) -> bool:
@@ -44,7 +47,7 @@ class ArrivalTime:
 
     def __sub__(self, other) -> 'ArrivalTime':
         other_time = self.__verify_data(other)
-        return ArrivalTime(self.arrival_time - other_time)
+        return ArrivalTime(self.arrival_time - self._datetime_to_timedelta(other_time))
 
     def __str__(self) -> str:
         if isinstance(self.arrival_time, timedelta):
