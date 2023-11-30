04ca62ee193e3bbbdcf3b22d3d8abd3b8fbb0261
kanewi11 <blacknekit11@gmail.com>
2023-11-30 12:11:15 +0300

    small improvements

--- airplane_arrival/airplane_arrival.py
+++ airplane_arrival/airplane_arrival.py
@@ -2,18 +2,22 @@ from .arrival_time import ArrivalTime
 
 
 class AirplaneArrival:
+    text_delayed = 'Самолет опаздывает. Задержка: {0}'
+    text_ahead = 'Самолет прилетел раньше. Опережение: {0}'
+    text_in_time = 'Самолет прилетел вовремя: {0}'
+
     def __init__(self, scheduled_arrival: str, actual_arrival: str):
         self.scheduled_arrival = ArrivalTime(scheduled_arrival)
         self.actual_arrival = ArrivalTime(actual_arrival)
 
-    def get_result(self):
+    def get_result(self) -> str:
         if self.actual_arrival > self.scheduled_arrival:
             delay = self.actual_arrival - self.scheduled_arrival
-            return f'Самолет опаздывает. Задержка: {delay}'
+            return self.text_delayed.format(delay)
         elif self.actual_arrival < self.scheduled_arrival:
             ahead_of_schedule = self.scheduled_arrival - self.actual_arrival
-            return f'Самолет прилетел раньше. Опережение: {ahead_of_schedule}'
-        return f'Самолет прилетел вовремя: {self.scheduled_arrival}'
+            return self.text_ahead.format(ahead_of_schedule)
+        return self.text_in_time.format(self.scheduled_arrival)
 
     def __str__(self):
         return f'Время по расписанию: {self.scheduled_arrival} | Фактическое время: {self.actual_arrival}'
